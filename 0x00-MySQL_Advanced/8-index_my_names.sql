bdfc9db73e60172f72187ea5634d53de09fcf4cc
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-11-03 20:05:55 +0100

    Sql advance

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Sql task 8:  Optimize simple search
 -- Write a SQL script that creates an index idx_name_first on the table names and the first letter of name.
 CREATE INDEX index_name_first
-    ON names(name);
+    ON names(name(1));
